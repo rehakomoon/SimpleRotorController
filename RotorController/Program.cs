1317d90c0b2efcd04b920c1ec0b87eb6d2275df2
rehakomoon <re.hako.moon@gmail.com>
2019-08-26 14:12:21 +0900

    First Commit

--- RotorController/Program.cs
+++ RotorController/Program.cs
@@ -8,8 +8,34 @@ namespace RotorController
 {
     class Program
     {
+        async static void SendMessageLoop()
+        {
+            RotorController rp = new RotorController("HakoDev_waki", 10);
+
+            var dummy_data = new byte[16];
+            dummy_data[0] = 255;
+
+            await rp.SetupAsync();
+
+            while (true)
+            {
+                for (byte i = 0; i <= 255; ++i)
+                {
+                    dummy_data[1] = i;
+                    await rp.SendMessage(dummy_data, false);
+                }
+            }
+
+        }
+
         static void Main(string[] args)
         {
+            var sm = Task.Run(() => SendMessageLoop());
+
+            while(true)
+            {
+                Task.Delay(1000);
+            }
         }
     }
 }
